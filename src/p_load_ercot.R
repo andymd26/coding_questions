93485883b02ef58b8708d4761f327873fc554ee8
Andrew Blohm <andymd26@Andrews-MacBook-Pro.local>
2017-03-10 10:34:18 -0500

    Resolved the coding question of how to integrate 'mle2' a maximum likelihood estimation package in R into the dplyr using a 'do()' wrapper.

--- src/p_load_ercot.R
+++ src/p_load_ercot.R
@@ -1,31 +1,28 @@
+install.packages("bbmle")
+install.packages("dplyr")
+
 rm(list = ls())
 options(scipen=999)
 options(digits=15)
-install.packages("bbmle")
-install.packages("dplyr")
 require(bbmle)
 require(dplyr)
 
-path = "~/coding_questions/data/"
+path = "~/Documents/coding_questions/data/"
 setwd(path)
+
+mle.func.lnorm = function(data) {
+  g = data$month_group[1]
+  hr = data$hour[1]
+  model = mle2(load~dlnorm(meanlog=mu, sdlog=sigma), start=(list(mu=mean(log(data$load)), sigma=sd(log(data$load)))), data=data)
+  c = model@details$convergence
+  ci.model = confint(model)
+  # Convergence code 1 means convergence failed
+  model.out = data.frame(group = g, hour = hr, convergence= c, mu.lb = ci.model[1], mu.ub = ci.model[3], 
+                         sigma.lb = ci.model[2], sigma.ub = ci.model[4])
+  return(model.out)
+}
+
 data.load = readRDS(file = "ercot_load.rds")
-
-LL = function(mu, sigma) {
-  R = dlnorm(data.load$load, mu, sigma)
-  loglik = -sum(log(R))
-  return(loglik)
-}
-# Log-likelihood function for the lognormal distribution
-m0 = mle2(LL, start=(list(mu=mean(log(data.load$load)), sigma=sd(log(data.load$load)))))
-m1 = confint(m0)
-# MLE lognormal parameter estimates for the entire set of data (but what we want is these parameter estimates for the different groups in the data)
-
-mle.func = function(data) {
-  model = mle2(LL(data=data), start=(list(mu=7, sigma=1)), data=list(data))
-  b = data.frame(mu.lb = a[1], mu.ub=a[2], sigma.lb=a[3], sigma.ub=a[4])
-  return(b)
-}
-
 data.load = data.load %>%
   filter(zone=="north") %>%
   # Filters out a representative region from the data
@@ -33,6 +30,7 @@ data.load = data.load %>%
   # Filters out the summer months
   filter(format(date, "%H") >= 15 & format(date, "%H") <= 22) %>%
   # Filters out 2pm to 9pm (numbers are hour ending)
+  # The above code filters out nonpeak load hours, as well as weekends.
   filter(weekdays(date)!= "Saturday" & weekdays(date)!="Sunday") %>%
   # Filters out weekdays
   mutate(month_group = ifelse(as.numeric(format(date, "%m"))==6 | as.numeric(format(date, "%m"))==9,1,2)) %>%
@@ -41,10 +39,5 @@ data.load = data.load %>%
   # Creates a new column hour for the group_by below (not necessary but creates a more attractive dataframe than the alternative).
   group_by(month_group, hour) %>%
   # Group_by is similar to subset. In this case I would like to calculate the mle parameters for the lognormal distribution (params = mu and sigma) for each hour (i.e., 2pm-9pm) for each month group (i.e., June/September and July/August).
-  do(confint(mle2(LL, start=(list(mu=mean(log(.$load)), sigma=sd(log(.$load)))))))
+  do(mle.func.lnorm(data=.))
   # The mle2 function requires two bits of information: log-likelihood function and initial values for all parameters in the log-likelihood function. It then optimizes the log-likelihood to find the most likely parameter estimates for the specified distribution. The issue is that mle2 returns an untidy object (if you've ever run a regression in R, mle2 produces similar output. 'confint' produces a n x 2 matrix (or dataframe, can't remember off the top of my head). Whereby n is the number of parameters being fit and the columns are the lower and upper bounds based on the alpha value (confidence level). Dplyr does not know what to do with a returned dataframe. One way to address this issue is to use the do() wrapper. Basically, for each group formed by the group_by command run the mle2, determine the confidence interval, and either add this data to the existing dataframe as new columns or generate a new dataframe. 
-  
-# The above code filters out nonpeak load hours, as well as weekends.
-# The load data is in the format 'hour ending' so the code above selects the hours from 2pm to 9pm
-
-
